cfb705298e436533d9bc816bebcd5018c4e888c7
石井康介 <faitayahhuu@yahoo.co.jp>
2019-01-05 00:08:55 +0900

    initial commit

--- pit.py
+++ pit.py
@@ -27,11 +27,11 @@ mcts1 = MCTS(g, n1, args1)
 n1p = lambda x: np.argmax(mcts1.getActionProb(x, temp=0))
 
 
-#n2 = NNet(g)
-#n2.load_checkpoint('/dev/8x50x25/','best.pth.tar')
-#args2 = dotdict({'numMCTSSims': 25, 'cpuct':1.0})
-#mcts2 = MCTS(g, n2, args2)
-#n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
+n2 = NNet(g)
+n2.load_checkpoint('./temp/','temp.pth.tar')
+args2 = dotdict({'numMCTSSims': 80, 'cpuct':2.0})
+mcts2 = MCTS(g, n2, args2)
+n2p = lambda x: np.argmax(mcts2.getActionProb(x, temp=0))
 
-arena = Arena.Arena(n1p, hp, g, display=display)
+arena = Arena.Arena(n1p, n2p, g, display=display)
 print(arena.playGames(2, verbose=True))
